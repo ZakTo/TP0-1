835dd9c56e4e43489f6f03f4c89f88ecc8f59e4e
Zakaria T. <zak.1540444@gmail.com>
2024-09-08 12:05:59 -0400

    math done

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
-rayon = 4
+r = int(input("Enter the radius in cm: "))
 
+A = math.pi * pow(r, 2)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Zakaria Toudji")
